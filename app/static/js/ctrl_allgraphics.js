bdc508001913f1b0f397686bf93d57b1a1609f5f
Jean Honlet <marielineet.jean@gmail.com>
2016-02-06 20:52:20 +0100

    Prune old files from previous project

--- app/static/js/ctrl_allgraphics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-mainApp.controller("ctrl_allGraphics", [ "$scope", function($scope) {
-  $scope.hovered = -1;
-  $scope.$on("hovered", function(event, i) {
-    $scope.hovered = i;
-  });
-}]);
