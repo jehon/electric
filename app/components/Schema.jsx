457533d0e0f49aa0c71004b72abec175351d0ab7
Jean Honlet <marielineet.jean@gmail.com>
2016-02-06 21:42:02 +0100

    Play with modifications

--- app/components/Schema.jsx
+++ app/components/Schema.jsx
@@ -17,7 +17,11 @@ export default class Schema extends React.Component {
         Hello world!
         <SVGDrawing height="1000" width="1000">
           <g transform="translate(100,100)">
-            <path d={'M0,0 L0,10 m-7.5,0 l15,0 m-20,7.5 l5,0 a7.5 7.5 0 0 1 15 0 l5,0'} stroke='red' fill='none' />
+            <g transform="scale(2)">
+              <g transform="rotate(0)">
+                <path d={'M0,0 L0,10 m-7.5,0 l15,0 m-20,7.5 l5,0 a7.5 7.5 0 0 1 15 0 l5,0'} stroke='red' fill='none' />
+              </g>
+            </g>
           </g>
           <line x1="100" y1="100" x2="200" y2="100" stroke="blue" />
           <rect x="87.5" y="100" width="25" height="25" stroke="green" fill='none' />
