68b18b945b2a9c24f96e22c26fa1bcfdfabb90c8
Jean Honlet <marielineet.jean@gmail.com>
2016-02-08 16:39:36 +0100

    Add xls parsing for references

---
+++ bin/reference.js
@@ -0,0 +1,21 @@
+
+xls2json = require("xls-to-json");
+fs = require("fs");
+
+
+xls2json({
+  'input': __dirname + "/../references.xls",
+  'output': null,
+  'sheet': 'elements'
+}, function(err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    var data = {};
+    for(var i in result) {
+      data[result[i].code] = result[i];
+    }
+    console.log(data);
+    fs.writeFileSync("references.js", "export default " + JSON.stringify(data));
+  }
+});
