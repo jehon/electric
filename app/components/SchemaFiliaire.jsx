0f9e8d00796f46cda95c6410f8da7057ba7cd499
Jean Honlet <marielineet.jean@gmail.com>
2016-02-11 22:32:37 +0100

    Add initial values

--- app/components/SchemaFiliaire.jsx
+++ app/components/SchemaFiliaire.jsx
@@ -7,14 +7,14 @@ export default class SchemaFiliaire extends React.Component {
   render() {
     var context = {
       color: 'green',
-      vertical: false
+      vertical: true
     };
 
     return (
       <div>
         <div>Toolbar and title</div>
         <SVGDrawing width={1000} height={300} >
-          <ElementFiliaire item={this.props.schema} context={context} x='800' y='200' />
+          <ElementFiliaire item={this.props.schema} context={context} x='50' y='300' />
         </SVGDrawing>
       </div>
     );
