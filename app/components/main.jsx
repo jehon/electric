80de30455b672c87b8cc9c88772ea92f5ccc0236
Jean Honlet <marielineet.jean@gmail.com>
2016-02-21 21:08:25 +0100

    Remove text area for json

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -7,13 +7,10 @@ import SchemaFiliaire   from 'components/SchemaFiliaire';
 import build            from 'helpers/build';
 
 var state = {};
-// var oschema = build(schema);
 
 fetch('/api/v0.1/test.php').then(function(response) {
   response.json().then(function(json) {
-    console.log(json);
     state.oschema = build(json);
-    console.log(state.oschema);
 
     ReactDOM.render(
       <SchemaFiliaire schema={state.oschema} />,
@@ -28,11 +25,6 @@ fetch('/api/v0.1/test.php').then(function(response) {
     ReactDOM.render(
       <SchemaPosition schema={state.oschema} image={{ src: 'http://localhost/rdc.png', width: 1089, height: 722, scale: 0.75 }} plan='rdc'/>,
       document.getElementById('plan_rdc')
-    );
-
-    ReactDOM.render(
-      <textarea value={JSON.stringify(state.oschema)} />,
-      document.getElementById('json')
     );
   });
 });
