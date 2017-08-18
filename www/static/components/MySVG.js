1d54cfb1e408fbf4907c2798e748ef4bc638c4a9
Jean Honlet <jean.honlet@oniryx.be>
2017-08-18 06:56:42 +0200

    Migrate to html component

--- www/static/components/MySVG.js
+++ www/static/components/MySVG.js
@@ -1,28 +1,48 @@
 
-class MySVG extends React.Component {
+class MySVG extends HTMLElement {
+  static get observedAttributes() { return ['value']; }
+
+  constructor() {
+    super();
+    this.title = "Define a title";
+    this.svgOptions = {
+      height: 800,
+      width: 600,
+    }
+  }
+
+  setSchema(schema) {
+    this.schema = schema;
+    this.render();
+  }
+
   render() {
-    console.log(this.props);
-    return (
+    console.log("render called");
+    this.innerHTML = `
       <div>
-        <div>{this.props.title}</div>
+        <div>${this.title}</div>
         <svg
-          {...this.props}
           preserveAspectRatio='xMinYMin slice'
           >
-          <rect x='0' y='0' width='100%' height='100%' fill='white' stroke='none'/>
-          {this.props.children}
+          <rect x='0' y='0' width='100%' height='100%' fill='white' stroke='red'/>
+          ${this.getChildren()}
         </svg>
       </div>
-    );
+    `;
+  }
+
+  getChildren() {
+    return "";
   }
 }
 
-MySVG.propTypes = {
-  title:     React.PropTypes.string,
-  height:    React.PropTypes.number,
-  width:     React.PropTypes.number,
-  children:  React.PropTypes.oneOfType([
-    React.PropTypes.arrayOf(React.PropTypes.node),
-    React.PropTypes.node
-  ])
-};
+
+// MySVG.propTypes = {
+//   title:     React.PropTypes.string,
+//   height:    React.PropTypes.number,
+//   width:     React.PropTypes.number,
+//   children:  React.PropTypes.oneOfType([
+//     React.PropTypes.arrayOf(React.PropTypes.node),
+//     React.PropTypes.node
+//   ])
+// };
