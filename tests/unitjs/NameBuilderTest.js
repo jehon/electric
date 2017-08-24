ce88b8c13d6a50139cdc918e8de8e051d3ab6ce6
Jean Honlet <jean.honlet@oniryx.be>
2017-08-24 21:14:38 +0200

    Test namebuilder with id

--- tests/unitjs/NameBuilderTest.js
+++ tests/unitjs/NameBuilderTest.js
@@ -9,6 +9,24 @@ describe("NameBuilderTest.js", function() {
 		expect(schema.getName()).toBe("A11");
 		expect(schema.next[0].getName()).toBe('A11.1');
 		expect(schema.next[0].next[0].getName()).toBe('A11.2');
+		expect(schema.next[0].next.length).toBe(1);
+		expect(schema.next[0].next[0].next[0].getName()).toBe('A11.3');
+		expect(schema.next[0].next[0].alternate[0].getName()).toBe('A11.2a.1');
+	});
+
+	it("should build correct names when id are set", function() {
+		IdBuilder.testResetUUID();
+		let schema = mockSimpleCircuit();
+		expect(schema).not.toBeNull();
+		let cb = new NameBuilder(schema);
+		let id = new IdBuilder(schema);
+		id.build();
+		cb.buildWithPrint();
+
+		expect(schema.getName()).toBe("A11");
+		expect(schema.next[0].getName()).toBe('A11.1');
+		expect(schema.next[0].next[0].getName()).toBe('A11.2');
+		expect(schema.next[0].next.length).toBe(1);
 		expect(schema.next[0].next[0].next[0].getName()).toBe('A11.3');
 		expect(schema.next[0].next[0].alternate[0].getName()).toBe('A11.2a.1');
 	});
