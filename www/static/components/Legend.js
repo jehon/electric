69fe0c4b9a400538b66a7cfe238f45aaf6894208
Jean Honlet <jean.honlet@oniryx.be>
2017-08-27 22:34:52 +0200

    Align correctly into position

--- www/static/components/Legend.js
+++ www/static/components/Legend.js
@@ -22,15 +22,19 @@ class Legend extends HTMLElement {
   }
 
   render() {
-    let schema = { type: this.value, options: this.options };
+    let schema = { type: this.value, options: this.options, x: 10, y: 20 };
     // (new IdBuilder(schema)).build();
     let builder = new PositionBuilder(schema);
+    let ref = drawReference(this.value);
+
+
     this.innerHTML = `
       <div>HHH
         <div>${this.value}</div>
-        <svg preserveAspectRatio='xMinYMin slice'>
-          <rect x='0' y='0' width='100%' height='100%' fill='white' stroke='red'/>
+        <svg preserveAspectRatio='xMinYMin slice' width="${ref.width + 40}" height="${ref.height + 20}">
             <g stroke="black">
+              <line x1=-100 y1=20 x2=100 y2=20 stroke="red" />
+              <line x1=10 y1=-100 x2=10 y2=100 stroke="red" />
                 ${builder.build()}
             </g>
         </svg>
