f3b70f003467f51a1ade46117c3d068418565340
Jean Honlet <jean.honlet@oniryx.be>
2017-08-28 21:52:40 +0200

    Show legend in tables

--- www/static/components/Legend.js
+++ www/static/components/Legend.js
@@ -29,16 +29,19 @@ class Legend extends HTMLElement {
 
 
     this.innerHTML = `
-      <div>HHH
-        <div>${this.value}</div>
-        <svg preserveAspectRatio='xMinYMin slice' width="${ref.width + 40}" height="${ref.height + 20}">
-            <g stroke="black">
-              <line x1=-100 y1=20 x2=100 y2=20 stroke="red" />
-              <line x1=10 y1=-100 x2=10 y2=100 stroke="red" />
-                ${builder.build()}
-            </g>
-        </svg>
-      </div>
+        <div class="row">
+            <div class="col-md-3">${this.value}</div>
+            <div class="col-md-2">${JSON.stringify(this.options)}</div>
+            <div class="col-md-7">
+                <svg preserveAspectRatio='xMinYMin slice' width="${ref.width + 40}" height="${ref.height + 20}">
+                    <g stroke="black">
+                      <line x1=-100 y1=20 x2=100 y2=20 stroke="red" />
+                      <line x1=10 y1=-100 x2=10 y2=100 stroke="red" />
+                      ${builder.build()}
+                    </g>
+                </svg>
+            </div>
+        </div>
     `;
   }
 }
