abdbd6870988923923b5d2dcea9c4211eeedab1a
Jean Honlet <jean.honlet@oniryx.be>
2017-08-30 22:46:18 +0200

    Enhance display

--- www/static/components/Legend.js
+++ www/static/components/Legend.js
@@ -30,9 +30,9 @@ class Legend extends HTMLElement {
 
     this.innerHTML = `
         <div class="row">
-            <div class="col-md-3">${this.value}</div>
+            <div class="col-md-1">${this.value}</div>
             <div class="col-md-2">${JSON.stringify(this.options)}</div>
-            <div class="col-md-3">
+            <div class="col-md-1">
                 <svg preserveAspectRatio='xMinYMin slice' width="${ref.width + 40}" height="${ref.height + 20}">
                     <g stroke="black">
                       <line x1=-100 y1=20 x2=100 y2=20 stroke="red" />
@@ -41,7 +41,7 @@ class Legend extends HTMLElement {
                     </g>
                 </svg>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-8">
               <pre>${JSON.stringify(drawReference(this.value), null, 2)}</pre>
             </div>
         </div>
