8598b84cbc7f4592b3a3ab946a88f6ed665158ee
Jean Honlet <jean.honlet@oniryx.be>
2017-09-04 20:11:55 +0200

    Add a migration handler

---
+++ www/static/class/MigrationBuilder.js
@@ -0,0 +1,18 @@
+
+MigrationBuilder = (function() {
+	class MigrationBuilder extends Builder {
+		buildSelf(...args) {
+			// Integrate options directly here
+			if ("options" in this) {
+				Object.assign(this._currentElement, this._currentElement.options);	
+			}
+			if ("reference" in this._currentElement) {
+				this._currentElement.name = this._currentElement.reference;
+			}
+			this._currentElement.orientation = ("orientation" in this._currentElement ? this._currentElement.orientation : 0 ) + 90;
+			return super.buildSelf(...args);
+		}
+	}
+
+	return MigrationBuilder;
+})();
