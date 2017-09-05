45597c4985a3853027584ce85ed15919e21db612
Jean Honlet <jean.honlet@oniryx.be>
2017-09-05 20:16:58 +0200

    Comment out dead code

--- www/static/helpers/away.js
+++ www/static/helpers/away.js
@@ -20,20 +20,20 @@ function away(w, h, angle) {
       // return d * Math.cos(angle / 180 * Math.PI);
     },
 
-    alignmentH: function() {
-      if (angle < 45)  { return 'middle'; }
-      if (angle < 135) { return 'end'; }
-      if (angle < 225) { return 'middle'; }
-      if (angle < 325) { return 'start'; }
-      return 'middle';
-    },
+    // alignmentH: function() {
+    //   if (angle < 45)  { return 'middle'; }
+    //   if (angle < 135) { return 'end'; }
+    //   if (angle < 225) { return 'middle'; }
+    //   if (angle < 325) { return 'start'; }
+    //   return 'middle';
+    // },
 
-    alignementV: function() {
-      if (angle < 45)  { return '+1em'; }
-      if (angle < 135) { return '+0.4em'; }
-      if (angle < 225) { return ''; }
-      if (angle < 325) { return '+0.4em'; }
-      return 'middle';
-    }
+    // alignementV: function() {
+    //   if (angle < 45)  { return '+1em'; }
+    //   if (angle < 135) { return '+0.4em'; }
+    //   if (angle < 225) { return ''; }
+    //   if (angle < 325) { return '+0.4em'; }
+    //   return 'middle';
+    // }
   };
 }
