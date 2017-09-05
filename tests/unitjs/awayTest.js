c8af6bca57204aeccfb59ffbb1295c86a80a9aee
Jean Honlet <jean.honlet@oniryx.be>
2017-09-05 20:14:40 +0200

    Fix away calculation

---
+++ tests/unitjs/awayTest.js
@@ -0,0 +1,29 @@
+
+describe("calculate away corretly", function() {
+	let w = 10;
+	let h = 100;
+
+	it("should find at 0°", function() {
+		let tp = away(w, h, 0);
+		expect(tp.x).toBeCloseTo(w, 0);
+		expect(tp.y).toBeCloseTo(h/2, 0);
+	});
+
+	it("should find at 90°", function() {
+		let tp = away(w, h, 90);
+		expect(tp.x).toBeCloseTo(h/2, 0);
+		expect(tp.y).toBeCloseTo(w, 0);
+	});
+
+	it("should find at 180°", function() {
+		let tp = away(w, h, 180);
+		expect(tp.x).toBeCloseTo(0, 0);
+		expect(tp.y).toBeCloseTo(h/2, 0);
+	});
+
+	it("should find at 270°", function() {
+		let tp = away(w, h, 270);
+		expect(tp.x).toBeCloseTo(50, 0);
+		expect(tp.y).toBeCloseTo(0, 0);
+	});
+});
--- www/static/helpers/away.js
+++ www/static/helpers/away.js
@@ -1,5 +1,5 @@
 
-function away(d, angle) {
+function away(w, h, angle) {
   // Normalize angle in [O, 360[
   while (angle < 0) {
     angle += 360;
@@ -8,12 +8,16 @@ function away(d, angle) {
     angle -= 360;
   }
 
+  let rad = angle / 180 * Math.PI;
+
   return {
     get x() {
-      return -d * Math.sin(angle / 180 * Math.PI);
+      return Math.max(0, w * Math.cos(rad) + Math.abs(h/2 * Math.sin(rad)));
+      // return -d * Math.sin(angle / 180 * Math.PI);
     },
     get y() {
-      return d * Math.cos(angle / 180 * Math.PI);
+      return Math.max(0, w * Math.sin(rad) + Math.abs(h/2 * Math.cos(rad)));
+      // return d * Math.cos(angle / 180 * Math.PI);
     },
 
     alignmentH: function() {
